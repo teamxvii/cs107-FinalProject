db7fd24b1b21dd9ebe21772ce1eb28b45c81cf99
mappia <gerald.arocena@gmail.com>
2020-12-01 23:28:07 -0500

    Checking changes

--- src/Elems.py
+++ src/Elems.py
@@ -9,6 +9,6 @@ def sin(x):
         der = {}
         for var in x._der.keys():
             der[var] = x.partial_der(var) * np.cos(x._val)
-        return Scalar(np.sin(x._val), der)
+        return Scalar(np.sin(x._val), der, parents=[x])
     except AttributeError:
         return np.sin(x)
--- src/Scalars.py
+++ src/Scalars.py
@@ -4,7 +4,7 @@ from FADiff import FADiff
 
 
 class Scalar:
-    def __init__(self, val, der=None, name=None, new_input=False):
+    def __init__(self, val, der=None, name=None, new_input=False, parents=[]):
         self._val = val
         if new_input:                      # Creating input var?
             self._der = {}
@@ -15,15 +15,19 @@ class Scalar:
             FADiff.vars_list.append(self)  # Add self to global vars list
         else:
             self._der = der
+        self.parents = set()
+        for parent in parents:
+            self.parents.add(parent)
 
     def __add__(self, other):
         try:
             der = {}
             for var, part_der in self._der.items():
                 der[var] = part_der + other.partial_der(var)
-            return Scalar(self._val + other._val, der)
+            parents = list(self.parents.add(self)) + list(other.parents.add(other))
+            return Scalar(self._val + other._val, der, parents=parents)
         except AttributeError:
-            return Scalar(self._val + other, self._der)
+            return Scalar(self._val + other, self._der, parents=[self])
 
     def __radd__(self, other):
         return self.__add__(other)
@@ -34,12 +38,12 @@ class Scalar:
             for var, part_der in self._der.items():
                 der[var] = self._val * other.partial_der(var) +\
                            part_der * other._val
-            return Scalar(self._val * other._val, der)
+            return Scalar(self._val * other._val, der, parents=[self, other])
         except AttributeError:
             der = {}
             for var, part_der in self._der.items():
                 der[var] = part_der * other
-            return Scalar(self._val * other, der)
+            return Scalar(self._val * other, der, parents=[self])
 
     def __rmul__(self, other):
         return self.__mul__(other)
@@ -51,14 +55,12 @@ class Scalar:
     def val(self):
         return self._val
 
-    @val.setter
-    def val(self, into):
-        self._val = into
-
     @property
     def der(self):
-        return self._der
-
-    @der.setter
-    def der(self, into):
-        self._der = into
+        parents = []
+        for key, value in self._der.items():
+            if key in self.parents:
+                parents.append(value)
+        if parents:
+            return parents
+        # TODO
