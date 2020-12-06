0f64690d94f33e0d808cf52a82d9347704a2b6f0
Elaine Cunha <elainecunha@Elaines-MacBook-Pro.local>
2020-12-06 15:54:07 -0500

    Updated FuncVect.py to handle vector functions of scalar inputs  and vector functions of a vector input. Note that we do not need to handle any case involving a function of multiple vector inputs, but we do need to handle the case of functions of a single vector input. Ask Gerald or me if you have questions.

--- src/fad/FuncVect.py
+++ src/fad/FuncVect.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python3
 
 from FADiff import FADiff
-from fad.Gradients import Scal as _fadScal
+from fad.Gradients import Scal as _fadscal
+from fad.Gradients import Vect as _fadvect
 
 
 class FuncVect:
     def __init__(self, funcs):
+        # check that all functions in vector are of same type (either all Scal or all Vect)
         try:
-            for func in funcs:
-                if type(func) != _fadScal:        # Don't accept non-Scals
-                    raise Exception('Invalid function type entered')
+            func_type = type(funcs[0]) # check the first element
+            for func in funcs[1:]:
+                if type(func) != func_type:        # Check for consistency
+                    raise Exception('All functions must be of same type (Scalar or Vector).')
         except Exception:
             raise
+        
+        # set inputs list based on type of functions
+        if scalar:
+            inputs = FADiff._fadscal_inputs
         else:
-            self._f_vect = funcs
-            self._input_vars = []     # Get complete list of input vars of f_vect
-            for func in funcs:
-                if func._parents:
-                    for var in func._der.keys():
-                        if var in func._parents:
-                            self._input_vars.append(var)
-                elif func in FADiff._fadscal_inputs:   # TODO: Input var (identity fxn) is correct? Make sure to test
-                    self._input_vars.append(func)
-            self._input_vars = list(set(self._input_vars))
+            inputs = FADiff._fadvect_inputs
+        
+        # create list of variables (either Scal or Vect objects) that are used in functions 
+        self._f_vect = funcs
+        self._input_vars = []     # Get complete list of input vars of f_vect
+        for func in funcs:
+            if func._parents:
+                for var in func._der.keys():
+                    if var in func._parents:
+                        self._input_vars.append(var)
+            elif func in inputs:   # TODO: Input var (identity fxn) is correct? Make sure to test
+                self._input_vars.append(func)
+        self._input_vars = list(set(self._input_vars))
 
     @property
     def val(self):
