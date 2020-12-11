099f73117cf46cfa3a121d6cf2e9efecf72a1b0e
liuxiaoxuan97 <54862568+liuxiaoxuan97@users.noreply.github.com>
2020-12-11 18:41:20 -0500

    Add files via upload

---
+++ FADiff/__init__.py
@@ -0,0 +1,5 @@
+from FADiff.FADiff import FADiff
+from fad.Gradients import Scal as fadScal
+from fad.Matrices import Vect as fadVect
+from rev.Gradients import Scal as revScal
+from rev.Matrices import Vect as revVect
